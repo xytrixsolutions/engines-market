4b0d3635b2d7460d998f812aa97368408b6ddd4c
muneeb <muneebusmani8355@gmail.com>
2025-08-06 19:04:06 +0500

    fixed aos on SSG

--- components/AOSInit.tsx
+++ components/AOSInit.tsx
@@ -1,21 +1,45 @@
-// src/components/AOSInit.tsx
+// // src/components/AOSInit.tsx
+// "use client";
+//
+// import { useEffect } from "react";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
+//
+// export default function AOSInit() {
+//   useEffect(() => {
+//     AOS.init({
+//       // duration: 1000,
+//       // once: true,
+//       duration: 500,
+//       easing: "ease-out-quart", // Smooth & fast
+//       once: true,
+//       offset: 50,
+//     });
+//   }, []);
+//
+//   return null;
+// }
+// app/components/AOSWrapper.tsx
 "use client";
 
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-export default function AOSInit() {
+const AOSWrapper = () => {
   useEffect(() => {
     AOS.init({
-      // duration: 1000,
-      // once: true,
-      duration: 500,
-      easing: "ease-out-quart", // Smooth & fast
+      duration: 800,
       once: true,
-      offset: 50,
     });
+
+    // refresh after short delay to ensure DOM is ready
+    setTimeout(() => {
+      AOS.refresh();
+    }, 500);
   }, []);
 
   return null;
-}
+};
+
+export default AOSWrapper;
